b8f089dbbdce22cf384f2cf55677719f73e8245b
Amitha Dissanayake <amitha@yaalalabs.com>
2025-07-28 18:46:28 +0530

    Import issues fixed
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>`

--- examples/travel-manager-agent/main.py
+++ examples/travel-manager-agent/main.py
@@ -1,10 +1,14 @@
 from ak_agents.orchestrator import OrchestratorAgent
+from ak_common.log.logger import get_logger
+
+log = get_logger("Main")
 
 
 def main():
     print("Hello from travel-manager-agent!")
     orchestrator = OrchestratorAgent()
-    orchestrator.get_error()
+    error = orchestrator.get_error()
+    log.info(f"Error Message: {error}")
 
 
 if __name__ == "__main__":
