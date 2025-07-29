479bfa4fbd2a6e65bbbc4ab38f94c1c5ea74b727
Tharindu Dissanayake <td@yaalalabs.com>
2025-07-29 11:40:15 +0530

    feat: support for crewai based agents
    Signed-off-by: Tharindu Dissanayake <td@yaalalabs.com>

---
+++ ak-py/ak-crewai/src/ak_crewai/__init__.py
@@ -0,0 +1,11 @@
+"""
+Agent Kernel support for CrewAI.
+
+This package contains Agent Kernel support for agents built with CrewAI.
+It provides the necessary classes and methods to integrate CrewAI agents into the Agent Kernel
+framework, allowing for seamless interaction and execution of CrewAI based agents.
+"""
+
+__version__ = "0.1.0"
+
+from .akcrewai import CrewAIModule as AgentModule
