f4e02e3accb191e2a68a1c1a1bc8177eced2e060
Amitha Dissanayake <amitha@yaalalabs.com>
2025-08-05 13:43:36 +0530

    New lambda example project
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- examples/aws-lambda-openai-agents/cli.py
+++
@@ -1,9 +0,0 @@
-from ak import CLI
-from ak_openai import AgentModule
-
-from agent import triage_agent, math_agent, history_agent
-
-AgentModule([triage_agent, math_agent, history_agent])
-
-if __name__ == "__main__":
-    CLI.main()
