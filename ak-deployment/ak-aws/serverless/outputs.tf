138ebc3a01a3d22eae14b6ba21f9c8043fbf7d38
Amitha Dissanayake <amitha@yaalalabs.com>
2025-08-05 20:05:37 +0530

    Refactoring

--- ak-deployment/ak-aws/serverless/outputs.tf
+++ ak-deployment/ak-aws/serverless/outputs.tf
@@ -10,6 +10,6 @@ output "lambda_function_invoke_arn" {
   value = module.lambda_deployment.lambda_function_invoke_arn
 }
 
-  output "agent_invoke_url" {
+output "agent_invoke_url" {
   value = "${aws_api_gateway_stage.stage.invoke_url}${aws_api_gateway_resource.agent_endpoint.path}"
 }
