f0a2eeca9154634ff9c1d3a764eddb2aebc2a7a3
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2025-08-07 12:26:30 +0530

    Feature/ak langgraph (#12)
    * base, remaining to do structed output from LLM
    
    * LangGraphAgent with improved handoff, logs and ChatLiteLLM structured output problem workaround
    
    * update .gitignore
    
    * Langgraph session using checkpointer
    
    * refactoring code
    
    * some bugfixes and improvements
    
    * log improvements
    
    * refactoring code part 2
    
    * refactoring ak-langgraph part 3
    
    * Refactoring
    
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>
    
    * refactoring session
    
    * fixed toml file
    
    * Minor refactoring
    
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>
    
    * More refactoring
    
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>
    
    * Fix copilot comments
    
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>
    
    * get state update
    
    ---------
    
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>
    Co-authored-by: Amitha Dissanayake <amitha@yaalalabs.com>

---
+++ ak-py/ak-langgraph/src/ak_langgraph/aklanggraph.py
@@ -0,0 +1,130 @@
+from typing import Any, Optional
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.state import CompiledStateGraph
+from pydantic import BaseModel
+from langchain_core.runnables.config import RunnableConfig
+
+from ak import Agent as BaseAgent, Module as BaseModule, Runner as BaseRunner, Session as BaseSession
+
+FRAMEWORK = "langgraph"
+
+
+class LangGraphSessionConfigurable(BaseModel):
+    thread_id: str
+
+
+class LangGraphSessionConfigModel(BaseModel):
+    configurable: LangGraphSessionConfigurable
+
+
+class LangGraphAgent(BaseAgent):
+    """
+    LangGraphAgent class provides an agent wrapping for LangGraph Agents SDK based agents.
+    """
+
+    def __init__(self, name: str, runner: 'LangGraphRunner', agent: CompiledStateGraph):
+        """
+        Initializes an LangGraphAgent instance.
+        :param name: Name of the agent.
+        :param runner: Runner associated with the agent.
+        :param agent: The LangGraph agent instance.
+        """
+        super().__init__(name, runner)
+        self._agent = agent
+
+    @property
+    def agent(self) -> CompiledStateGraph:
+        """
+        Returns the LangGraph CompiledStateGraph instance.
+        """
+        return self._agent
+
+
+class LangGraphSession(BaseSession):
+    """
+    LangGraphSession class provides a session for LangGraph Agents SDK-based agents,
+    without relying on a custom TypedDict like AgentState.
+    """
+
+    def __init__(self):
+        super().__init__(FRAMEWORK)
+        self._checkpointer = MemorySaver()
+
+    def _get_config(self) -> dict:
+        return LangGraphSessionConfigModel(configurable=LangGraphSessionConfigurable(thread_id=self.id)).model_dump()
+
+    async def get_state(self) -> dict:
+        """
+        Retrieve the current state from the checkpoint system.
+        Returns a dictionary with keys like 'messages'.
+        """
+        raw_config = self._get_config()
+        tup = await self._checkpointer.aget_tuple(RunnableConfig(configurable=raw_config.get("configurable", {})))
+
+        if tup is not None:
+            messages = tuple(tup.checkpoint.channel_values.get("messages", ()))
+        else:
+            messages = ()
+
+        return {
+            "messages": messages,
+        }
+
+    async def get_items(self, limit: Optional[int] = None) -> list[BaseMessage]:
+        """
+        Return list of BaseMessage in chronological order,
+        optionally truncated to the most recent `limit` messages.
+        """
+        state = await self.get_state()
+        messages = state["messages"]
+        return list(messages[-limit:] if limit else messages)
+
+
+class LangGraphRunner(BaseRunner):
+    """
+    LangGraphRunner class provides a runner for LangGraph Agents SDK based agents.
+    """
+
+    def __init__(self):
+        """
+        Initializes an LangGraphRunner instance.
+        """
+        super().__init__(FRAMEWORK)
+
+    def _session(self, session: BaseSession) -> LangGraphSession:
+        """
+        Returns the LangGraph session associated with the provided session.
+        :param session: The session to retrieve the LangGraph session for.
+        :return: LangGraphSession instance.
+        """
+        if session is None:
+            return None
+        return session.get(FRAMEWORK) or session.set(FRAMEWORK, LangGraphSession())
+
+    async def run(self, agent: LangGraphAgent, session: BaseSession, prompt: Any) -> Any:
+        session_config = LangGraphSessionConfigModel(
+            configurable=LangGraphSessionConfigurable(
+                thread_id=session.id
+            )
+        )
+        agent.agent.checkpointer = self._session(session)._checkpointer
+        result = await agent.agent.ainvoke(input={"messages": [HumanMessage(content=prompt)]},
+                                           config=session_config.model_dump())
+        last_message = result["messages"][-1]
+        return last_message.content
+
+
+class LangGraphModule(BaseModule):
+    """
+    LangGraphModule class provides a module for LangGraph Agent SDK based agents.
+    """
+
+    def __init__(self, agents: list[CompiledStateGraph]):
+        """
+        Initializes a LangGraphModule instance.
+        :param agents: List of agents in the module.
+        """
+        runner = LangGraphRunner()
+        super().__init__(list(map(lambda agent: LangGraphAgent(name=agent.name, runner=runner, agent=agent), agents)))
---
+++ examples/cli-langgraph-agents/README.md
@@ -0,0 +1,8 @@
+# Agent Kernel running LangGraph Agents
+
+This package contains a demo of Agent Kernel running agents built with LangGraph. Users may
+interact with agents via the Agent Kernel CLI.
+
+Run this demo using the following.
+
+    python demo.py
