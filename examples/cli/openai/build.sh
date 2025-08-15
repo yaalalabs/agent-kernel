f14645db37cf4ea6c54a7ac1fdc8220b18cd4cec
Amitha Dissanayake <amitha@yaalalabs.com>
2025-08-15 15:31:39 +0530

    Build script refactoring
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- examples/cli/openai/build.sh
+++ examples/cli/openai/build.sh
@@ -7,7 +7,6 @@ owner=110597261937
 region=ap-southeast-2
 repo=agent-kernel
 
-# Get a fresh CodeArtifact authorization token (expires in ~12h)
 AUTH_TOKEN=$(aws codeartifact get-authorization-token \
   --domain "$domain" \
   --domain-owner "$owner" \
@@ -15,14 +14,11 @@ AUTH_TOKEN=$(aws codeartifact get-authorization-token \
   --query authorizationToken \
   --output text)
 
-# Construct the authenticated simple index URL for CodeArtifact
 CA_SIMPLE_URL="https://aws:${AUTH_TOKEN}@${domain}-${owner}.d.codeartifact.${region}.amazonaws.com/pypi/${repo}/simple"
 
-# Tell uv/pip to use CodeArtifact as the primary index. Keep PyPI as an extra index via pyproject.
+# Tell uv/pip to use CodeArtifact as the extra index. Keep PyPI as the primary
 export UV_EXTRA_INDEX_URL="$CA_SIMPLE_URL"
 export PIP_EXTRA_INDEX_URL="$CA_SIMPLE_URL"
-
-#uv pip install ak[cli,openai]==0.1.0a1
 
 uv venv --allow-existing
 uv sync
