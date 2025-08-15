f185ebda4c4ec839781f8a3b54c91d22dbf0dc37
Tharindu Dissanayake <td@yaalalabs.com>
2025-08-15 10:53:49 +0530

    feat: refactoring and cleaning up examples
    Signed-off-by: Tharindu Dissanayake <td@yaalalabs.com>

---
+++ examples/aws-serverless/crewai/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+uv venv --allow-existing
+uv sync
+uv pip install --force-reinstall --find-links ../../../ak-py/dist ak # Required if you changed ak-agents after the first install
--- examples/cli-langgraph-agents/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-uv venv --allow-existing
-uv sync
-uv pip install --force-reinstall --find-links ../../ak-py/dist ak # Required if you changed ak after the first install
