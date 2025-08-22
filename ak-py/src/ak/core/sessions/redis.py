d96fa62c19bb1d93578cdd4f00f29ed563c8b8e8
Amitha Dissanayake <amitha@yaalalabs.com>
2025-08-22 13:10:34 +0530

    Refactoring
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

---
+++ ak-py/src/ak/core/sessions/redis.py
@@ -0,0 +1,59 @@
+import logging
+
+from .base import SessionStore
+from ..base import Session
+
+
+class RedisSessionStore(SessionStore):
+    """
+    InMemorySessionStore class provides an in-memory implementation of the SessionStore interface.
+    """
+
+    def __init__(self):
+        """
+        Initializes an InMemorySessionStore instance.
+        """
+        self._sessions = {}
+        self._log = logging.getLogger("ak.core.sessions.inmemory")
+
+    def load(self, session_id: str, strict: bool = False) -> Session:
+        """
+        Loads a session by its unique identifier.
+        :param session_id: Unique identifier for the session.
+        :param strict: If True, raises an exception if the session is not found.
+        :return: The session associated with the identifier, or a new session if it does not exist.
+        """
+        session = self._sessions.get(session_id)
+        if session is None:
+            if strict:
+                raise KeyError(f"Session {session_id} not found")
+            else:
+                self._log.warning(f"Session {session_id} not found, creating new session")
+                session = Session(session_id)
+                self._sessions[session_id] = session
+        return session
+
+    def new(self, session_id: str) -> Session:
+        """
+        Initialize a session for a given session id.
+        :param session_id: Unique identifier for the session.
+        :return: The session associated with the identifier, or a new session if it does not exist.
+        """
+        self._log.debug(f"Creating new session with ID {session_id} ")
+        session = Session(session_id)
+        self._sessions[session_id] = session
+
+        return session
+
+    def store(self, session: Session) -> None:
+        """
+        Stores a session or updates it if it already exists in the storage.
+        :param session: The session to store.
+        """
+        self._sessions[session.id] = session
+
+    def clear(self) -> None:
+        """
+        Clears all stored sessions.
+        """
+        self._sessions.clear()
