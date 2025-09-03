73d484a0f8ba473573e97057512c12e40eacaa3e
Amitha Dissanayake <amitha@yaalalabs.com>
2025-09-03 12:23:18 +0530

    Adding containered example
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

---
+++ examples/aws-containered/crewai/deploy/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create a zip file of the Lambda function code
+create_deployment_package() {
+    pushd ../
+    rm -rf dist
+    mkdir -p dist/data
+    uv export --no-hashes > requirements.txt
+    if [[ ${1-} != "local" ]]; then
+      uv pip install -r requirements.txt --target=dist/data
+    else
+      uv pip install -r requirements.txt --target=dist/data  --find-links ../../../ak-py/dist
+      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist ak || true
+    fi
+    cp -r lambda.py dist/data
+    popd || exit 1
+    cp Dockerfile ../dist/
+}
+
+create_deployment_package $1
+
+terraform init
+terraform apply
