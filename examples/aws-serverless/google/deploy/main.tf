ed1b74f77652dde706648bc2a403dc58425ef605
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2025-09-19 20:40:48 +0530

    .changed package to image

--- examples/aws-serverless/google/deploy/main.tf
+++ examples/aws-serverless/google/deploy/main.tf
@@ -10,11 +10,14 @@ module "serverless_agents" {
   function_name        = "google-agents"
   handler_path         = "lambda.handler"
   module_name          = var.module_name
-  package_path         = "../dist.zip"
-  memory_size          = 256
+  package_path         = "../dist"
+  memory_size          = 512
   create_redis_cluster = true
   product_display_name = "AK Google Serverless Example"
+  package_type         = "Image"
   region               = var.region
+  vpc_id               = "vpc-09033229d67314c1c"
+  private_subnet_ids   = ["subnet-00e888e445f16d1b1", "subnet-0ab5240262cd77119"]
 
   # Environment variables passed to lambda
   environment_variables = {
