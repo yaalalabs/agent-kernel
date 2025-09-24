4a7d6fd346376e00052542b4867719ddf7b2a8d0
Amitha Dissanayake <amitha@yaalalabs.com>
2025-09-24 20:18:37 +0530

    Fixed the test
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- examples/api/a2a/multi/a2a_test.py
+++ examples/api/a2a/multi/a2a_test.py
@@ -1,37 +1,40 @@
+# test_app.py
+import asyncio
+import subprocess
+import sys
+
 import pytest
 import pytest_asyncio
 from ak.test import Test
 
 from client import A2AHttpClient
 
-pytestmark = pytest.mark.asyncio(loop_scope="function")  # uses a single session for all tests
 
+@pytest_asyncio.fixture(scope="session")
+async def a2a_client():
+    """Start the FastAPI server as a subprocess and stop it after tests."""
+    proc = subprocess.Popen(
+        ["python3", "server.py"],
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+    await asyncio.sleep(15)
+    client = A2AHttpClient(base_url="http://127.0.0.1:8000/a2a/history")
+    await client.init()
 
-@pytest_asyncio.fixture(scope="function", loop_scope="function")
-async def test_server():
-    test = Test("server.py", cli_mode=False)
-    await test.start()
-    print("Server started")
     try:
-        yield test
+        yield client
     finally:
-        await test.stop()
+        proc.terminate()
+        proc.wait()
 
 
-@pytest_asyncio.fixture(scope="function", loop_scope="function")
-async def test_client(test_server):
-    test_client = A2AHttpClient("http://127.0.0.1:8000/a2a/general")
-    await test_client.init()
-    print("Client initialized")
-    try:
-        yield test_client
-    finally:
-        pass
+@pytest.mark.asyncio
+async def test_call_api(a2a_client):
+    test = Test("server.py")
 
+    response = await a2a_client.send("Who won the 1996 cricket world cup?")
+    test.compare(response, "Sri Lanka won the 1996 cricket world cup.")
 
-@pytest.mark.order(1)
-async def test_first_question(test_server, test_client):
-    response = await test_client.send("Who won the 1996 cricket world cup?")
-    test_server.compare(response, "Sri Lanka won the world cup.")
-    response = await test_client.send("Which countries hosted the tournament?")
-    test_server.compare(response, "The 1996 Cricket World Cup was hosted by India, Pakistan, and Sri Lanka.")
+    response = await a2a_client.send("Which countries hosted the tournament?")
+    test.compare(response, "The 1996 Cricket World Cup was hosted by India, Pakistan, and Sri Lanka.")
