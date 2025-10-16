0dc0c8a2095b39c0f8eaaa1d17d8787e0a20d4b7
Amitha Dissanayake <amitha@yaalalabs.com>
2025-10-16 19:42:31 +0530

    feat: add custom domain for development
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://yaalalabs.github.io',
+  url: 'https://kernel.yaala.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/agent-kernel/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
