ee73d0f225b510af157a1d4f8a7e1981224a97eb
Amitha Dissanayake <amitha@yaalalabs.com>
2025-10-16 19:50:36 +0530

    Fix dependencies
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -25,7 +25,6 @@ const config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -37,6 +36,9 @@ const config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
   themes: ['@docusaurus/theme-mermaid'],
 
