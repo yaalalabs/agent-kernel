574adc8e05d54aba9ab738a433d769787e42f513
Amitha Dissanayake <amitha@yaalalabs.com>
2025-11-25 12:26:49 +0530

    fix: lint
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- ak-py/src/agentkernel/api/__init__.py
+++ ak-py/src/agentkernel/api/__init__.py
@@ -11,6 +11,5 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.1.0"
 
-from .handler import AgentRESTRequestHandler
-from .handler import RESTRequestHandler
+from .handler import AgentRESTRequestHandler, RESTRequestHandler
 from .http import RESTAPI
