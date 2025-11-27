1bade8d6c86c43865f1e6f5d90370643e58c9c41
Amitha Dissanayake <amitha@yaalalabs.com>
2025-11-27 12:25:41 +0530

    Fix dependency issues
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- examples/aws-containerized/openai-dynamodb/deploy/deploy.sh
+++ examples/aws-containerized/openai-dynamodb/deploy/deploy.sh
@@ -10,7 +10,7 @@ create_deployment_package() {
       uv pip install -r requirements.txt --target=dist/data
     else
       uv pip install -r requirements.txt --target=dist/data  --find-links ../../../ak-py/dist
-      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist agentkernel[adk,api,redis] || true
+      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist agentkernel[adk,api,aws] || true
     fi
     cp -r app.py config.yaml dist/data
     popd || exit 1
