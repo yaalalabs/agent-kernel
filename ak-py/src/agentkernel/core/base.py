bbcddce4a855c7dc46ca18058637756c633abc34
manoj <manoj@yaalalabs.com>
2025-11-28 09:55:54 +0530

    defs updated

--- ak-py/src/agentkernel/core/base.py
+++ ak-py/src/agentkernel/core/base.py
@@ -65,14 +65,22 @@ class Session:
 class AgentRequestText(BaseModel):
     """
     AgentRequestText encapsulates a text request to an agent.
+        
+    prompt: str  : This is the user input text
+    type: Literal["text"]
     """
     prompt: str
     type: Literal["text"]
 class AgentRequestFile(BaseModel):
     """
     AgentRequestFile encapsulates a file attachment request to an agent
+    
+    file_data: str  : This could be base64 encoded string or url
+    name: str : name of the file
+    type: Literal["file"]
+    mime_type: str | None = None : Optional MIME The IANA standard MIME type of
     """
-    file_data: bytes | str  # This could be base64 encoded string or bytes or url
+    file_data: str  # This could be base64 encoded string or url
     name: str
     type: Literal["file"]
     mime_type: str | None = None # Optional MIME The IANA standard MIME type of the source data
@@ -80,17 +88,27 @@ class AgentRequestFile(BaseModel):
 class AgentRequestImage(BaseModel):
     """
     AgentRequestImage encapsulates an image request to an agent
+    
+    image_data: str  : This should be base64 encoded string
+    name: str : name of the image
+    type: Literal["image"]
+    mime_type: str | None = None : Optional MIME The IANA standard MIME type of
     """
-    image_data: bytes | str  # This could be base64 encoded string or bytes or url
+    image_data: str
     name: str
     type: Literal["image"]
-    mime_type: str | None = None # Optional MIME The IANA standard MIME type of the source data
+    mime_type: str | None = None 
 
 class AgentRequestAny(BaseModel):
     """
     AgentRequestAny encapsulates a passing any type of request to be handled by the pre-execution hooks. These are not directly handled by the agent kernel runtime.
+   
+    any_data: Any : This could be base64 encoded string or bytes or url
+    name: str : name of the data
+    type: Literal["other"]
     """
-    additional_context: Any | None = None
+    any_data: Any
+    name: str
     type: Literal["other"]
     
 AgentRequest = Union[AgentRequestText, AgentRequestFile, AgentRequestImage, AgentRequestAny]
