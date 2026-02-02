a537654a62a8398e780f1f2afdd417c2478a2ed6
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-02-02 13:52:26 +0530

    added the auth outputs to Openai examples output.tf

--- examples/aws-serverless/openai/deploy/outputs.tf
+++ examples/aws-serverless/openai/deploy/outputs.tf
@@ -1,3 +1,8 @@
+output "authorizer_status" {
+  description = "Status message indicating whether the authorizer Lambda will be created"
+  value       = module.serverless_agents.authorizer_status
+}
+
 output "agent_invoke_url" {
   description = "The URL to invoke the agent lambda function"
   value       = module.serverless_agents.agent_invoke_url
