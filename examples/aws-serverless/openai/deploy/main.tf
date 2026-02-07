363a6ca10454d85120f03fc3c085d0d6123eb99c
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-02-07 17:31:47 +0530

    openai deployment main.tf file change

--- examples/aws-serverless/openai/deploy/main.tf
+++ examples/aws-serverless/openai/deploy/main.tf
@@ -1,8 +1,7 @@
 # Lambda module configuration for deploying OpenAI Agent Lambda function
 module "serverless_agents" {
-  # source = "yaalalabs/ak-serverless/aws"
-  # version = "0.2.11"
-  source = "../../../../ak-deployment/ak-aws/serverless"
+  source = "yaalalabs/ak-serverless/aws"
+  version = "0.2.11"
 
   # Basic lambda configuration
   product_alias        = var.product_alias
@@ -39,16 +38,4 @@ module "serverless_agents" {
   environment_variables = {
     "OPENAI_API_KEY" = var.openai_api_key
   }
-  
-  # # Defining the API Gateway Authorizer
-  # authorizer_function_description = "API Gateway Lambda Authorizer 2"
-  # authorizer_function_name = "gtwy-auth"
-  # authorizer_handler_path = "lambda.handler"
-  # authorizer_package_path = "../auth_deployment/auth_dist.zip"
-  # authorizer_package_type = "S3Zip"
-  # authorizer_module_name = var.authorizer_module_name
-  # authorizer_result_ttl_in_seconds = 0
-  # authorizer_environment_variables = {
-  #   "SOME_OTHER_KEY" = "Some Other Value"
-  # }
 } 
