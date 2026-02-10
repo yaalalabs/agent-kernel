16178d5c25b9179be4a8f27512cf4be04c484454
Amitha Dissanayake <amitha@yaalalabs.com>
2026-02-10 13:34:20 +0530

    Update agent
    Signed-off-by: Amitha Dissanayake <amitha@yaalalabs.com>

--- docs/src/components/FloatingChatbot/index.tsx
+++ docs/src/components/FloatingChatbot/index.tsx
@@ -85,7 +85,7 @@ const FloatingChatbot: React.FC = () => {
     }]);
 
     try {
-      const response = await fetch('https://4h9x2girbi.execute-api.ap-southeast-2.amazonaws.com/agents/api/v1/chat', {
+      const response = await fetch('https://63e0emv8qf.execute-api.ap-southeast-1.amazonaws.com/agents/api/v1/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
