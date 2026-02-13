509f8697b99547794d8b9b81c31336616d8fe811
yl-nuwan <nuwan.udara@yaalalabs.com>
2026-02-13 17:48:57 +0530

    Add Azure Cosmos DB memory backend example for Agent Kernel

---
+++ examples/memory/cosmos/deploy/outputs.tf
@@ -0,0 +1,24 @@
+output "function_app_url" {
+  description = "Function App default hostname"
+  value       = module.FlexFunction.function_app_url
+}
+
+output "function_app_id" {
+  description = "Function App resource ID"
+  value       = module.FlexFunction.function_app_id
+}
+
+output "function_app_name" {
+  description = "Function App name"
+  value       = module.FlexFunction.function_app_name
+}
+
+output "api_management_gateway_url" {
+  description = "API Management gateway URL"
+  value       = module.FlexFunction.api_management_gateway_url
+}
+
+output "api_url"{
+  description = "Complete API URL with base path and version"
+  value       = module.FlexFunction.api_url
+}
---
+++ examples/memory/cosmos/deploy/provision.sh
@@ -0,0 +1,1 @@
+../../../../scripts/provision.sh
