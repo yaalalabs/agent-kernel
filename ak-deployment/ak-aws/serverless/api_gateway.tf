12421182d6e467afe81d7ea7b16801c26ee5de88
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-02-14 14:20:59 +0530

    Revert "getting rid of the stage vars"
    This reverts commit e856229e0fa44250e29f27f6cb77af16c4d71cc0.

--- ak-deployment/ak-aws/serverless/api_gateway.tf
+++ ak-deployment/ak-aws/serverless/api_gateway.tf
@@ -157,7 +157,13 @@ resource "aws_api_gateway_stage" "stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   stage_name    = "agents"
-  
+
+  variables = {
+    api_base_path  = var.api_base_path
+    api_version    = var.api_version
+    agent_endpoint = var.agent_endpoint
+  }
+
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway.arn
     format = jsonencode({
