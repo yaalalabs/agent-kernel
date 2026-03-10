83a55c52db2fdd050d62ad3e9df01c6a3ebbcc62
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-03-10 14:21:16 +0530

    new changes

--- examples/api/multimodal/redis/deploy/deploy.sh
+++ examples/api/multimodal/redis/deploy/deploy.sh
@@ -3,14 +3,14 @@
 # Create a deployment package for container image
 create_deployment_package() {
     pushd ../
-    rm -rf dist 
+    rm -rf dist
     mkdir -p dist/data
     uv export --no-dev --no-hashes > requirements.txt
     if [[ ${1-} != "local" ]]; then
       uv pip install -r requirements.txt --target=dist/data
     else
-      uv pip install -r requirements.txt --target=dist/data  --find-links ../../../ak-py/dist
-      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist agentkernel[openai,redis,aws] || true
+      uv pip install -r requirements.txt --target=dist/data --find-links ../../../../ak-py/dist 
+      uv pip install --force-reinstall --target=dist/data --find-links ../../../../ak-py/dist agentkernel[openai,redis,aws] || true
     fi
     cp -r lambda.py config.yaml dist/data
     popd || exit 1
