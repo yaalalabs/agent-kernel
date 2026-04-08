d27cba8921b7ca64a82a94d4c275fee0441dc17a
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-04-08 18:38:00 +0530

    removed sqs handler form init file for consistancy

--- ak-py/src/agentkernel/deployment/aws/core/__init__.py
+++ ak-py/src/agentkernel/deployment/aws/core/__init__.py
@@ -1,3 +1,1 @@
 # This core folder will contain the core components of the AWS deployment (for both Serverless & Containerized), such as models, utilities, base classes for services, etc.
-
-from .sqs_handler import SQSHandler
