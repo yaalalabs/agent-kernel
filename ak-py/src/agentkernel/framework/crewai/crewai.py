fb2248c2c456c4ef89b67f003b11fe6085e29d3c
JohnPraveenYL <john.praveen@yaalalabs.com>
2026-04-22 14:50:09 +0530

    fix: remove comment

--- ak-py/src/agentkernel/framework/crewai/crewai.py
+++ ak-py/src/agentkernel/framework/crewai/crewai.py
@@ -51,7 +51,6 @@ class CrewAISession(Storage):
         :return: List of items matching the query.
         """
         self._log.debug(f"search: {query}, {limit}, {score_threshold}")
-        #added content for backward compatibiliy
         return list(map(lambda item: {"content": item["value"], "context": item["value"]}, self._items[:limit]))
 
     def reset(self) -> None:
