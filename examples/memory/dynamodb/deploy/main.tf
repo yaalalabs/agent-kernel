5da197d01c607f819edd2754506570bcbeba3ffe
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-04-25 18:55:27 +0530

    request_handler object addtion examples updates

--- examples/memory/dynamodb/deploy/main.tf
+++ examples/memory/dynamodb/deploy/main.tf
@@ -6,21 +6,24 @@ module "serverless_agents" {
   # Basic lambda configuration
   product_alias                = var.product_alias
   env_alias                    = var.env_alias
-  function_description         = "Agent Kernel OpenAI with DynamoDB"
-  function_name                = "oai-ddb"
-  handler_path                 = "lambda.handler"
   module_name                  = var.module_name
-  package_path                 = "../dist"
-  package_type                 = "Image"
-  memory_size                  = 256
   product_display_name         = "Agent Kernel OpenAI with DynamoDB"
   create_dynamodb_memory_table = true
   vpc_id                       = "vpc-09033229d67314c1c"
   private_subnet_ids           = ["subnet-00e888e445f16d1b1", "subnet-0ab5240262cd77119"]
   region                       = var.region
 
-  # Environment variables passed to lambda
-  environment_variables = {
-    "OPENAI_API_KEY" = var.openai_api_key
+  # Request handler configuration
+  request_handler = {
+    function_description         = "Agent Kernel OpenAI with DynamoDB"
+    function_name                = "oai-ddb"
+    handler_path                 = "lambda.handler"
+    module_name                  = var.module_name
+    package_path                 = "../dist"
+    package_type                 = "Image"
+    memory_size                  = 256
+    environment_variables = {
+      "OPENAI_API_KEY" = var.openai_api_key
+    }
   }
 }
--- examples/memory/redis/deploy/main.tf
+++ examples/memory/redis/deploy/main.tf
@@ -6,21 +6,24 @@ module "serverless_agents" {
   # Basic lambda configuration
   product_alias        = var.product_alias
   env_alias            = var.env_alias
-  function_description = "Agent Kernel OpenAI with Redis"
-  function_name        = "oai-redis"
-  handler_path         = "lambda.handler"
   module_name          = var.module_name
-  package_path         = "../dist"
-  package_type         = "Image"
-  memory_size          = 256
-  create_redis_cluster = false # This is optional. Set to true if you want to create one. Otherwise you can reuse an already existing redis host by setting configurations on config.yaml
   product_display_name = "Agent Kernel OpenAI with Redis"
+  create_redis_cluster = false # This is optional. Set to true if you want to create one. Otherwise you can reuse an already existing redis host by setting configurations on config.yaml
   vpc_id               = "vpc-09033229d67314c1c"
   private_subnet_ids   = ["subnet-00e888e445f16d1b1", "subnet-0ab5240262cd77119"]
   region               = var.region
 
-  # Environment variables passed to lambda
-  environment_variables = {
-    "OPENAI_API_KEY" = var.openai_api_key
+  # Request handler configuration
+  request_handler = {
+    function_description = "Agent Kernel OpenAI with Redis"
+    function_name        = "oai-redis"
+    handler_path         = "lambda.handler"
+    module_name          = var.module_name
+    package_path         = "../dist"
+    package_type         = "Image"
+    memory_size          = 256
+    environment_variables = {
+      "OPENAI_API_KEY" = var.openai_api_key
+    }
   }
 }
