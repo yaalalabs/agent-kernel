c9fd522c672cf21444b80bf12a9aed574f923751
JohnPraveenYL <john.praveen@yaalalabs.com>
2026-05-12 16:08:54 +0530

    fix: update Redis connection URL in config and streamline deployment package script

--- examples/aws-containerized/crewai/deploy/deploy.sh
+++ examples/aws-containerized/crewai/deploy/deploy.sh
@@ -9,8 +9,7 @@ create_deployment_package() {
     if [[ ${1-} != "local" ]]; then
       uv pip install -r requirements.txt --target=dist/data
     else
-      uv pip install -r requirements.txt --target=dist/data  --find-links ../../../ak-py/dist
-      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist agentkernel[api,crewai,redis,test] || true
+      uv pip install -r requirements.txt --target=dist/data --find-links ../../../ak-py/dist
     fi
     cp -r app.py config.yaml dist/data
     popd || exit 1
