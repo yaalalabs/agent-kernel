b226bc91655b6c9582eac346462f453305aa64ba
JohnPraveenYL <john.praveen@yaalalabs.com>
2026-05-12 15:15:57 +0530

    fix: update build scripts to ensure proper Redis installation for serverless deployments

--- examples/aws-serverless/crewai/build.sh
+++ examples/aws-serverless/crewai/build.sh
@@ -11,5 +11,6 @@ if [[ ${1-} != "local" ]]; then
   uv sync --all-extras
 else
   # For local development of agentkernel, you can force reinstall from local dist
-    uv sync --force-reinstall --find-links ../../../ak-py/dist --all-extras
+  uv sync --find-links ../../../ak-py/dist --all-extras
+  uv pip install --force-reinstall --find-links ../../../ak-py/dist agentkernel[redis,crewai,test] || true
 fi
