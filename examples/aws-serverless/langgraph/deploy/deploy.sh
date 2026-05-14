ee7c97b281b7f05c6e4582f42841d8434a81a6d7
JohnPraveenYL <john.praveen@yaalalabs.com>
2026-05-14 12:50:03 +0530

    fix: update deployment scripts to force reinstall agentkernel dependencies for Redis support

--- examples/aws-serverless/langgraph/deploy/deploy.sh
+++ examples/aws-serverless/langgraph/deploy/deploy.sh
@@ -9,7 +9,8 @@ create_deployment_package() {
     if [[ ${1-} != "local" ]]; then
       uv pip install -r requirements.txt --target=dist/data
     else
-      uv pip install -r requirements.txt --target=dist/data --find-links ../../../ak-py/dist
+      uv pip install -r requirements.txt --target=dist/data  --find-links ../../../ak-py/dist
+      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist agentkernel[langgraph,redis,test] || true
     fi
     cp -r lambda.py custom_agent.py config.yaml dist/data
     popd || exit 1
