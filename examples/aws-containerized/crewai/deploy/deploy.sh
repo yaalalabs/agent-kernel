418610b056eac83ef449a9e994c1fa0285b63182
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-06-05 14:21:13 +0530

    added multimodal to crewai test

--- examples/aws-containerized/crewai/deploy/deploy.sh
+++ examples/aws-containerized/crewai/deploy/deploy.sh
@@ -10,7 +10,7 @@ create_deployment_package() {
       uv pip install -r requirements.txt --target=dist/data
     else
       uv pip install -r requirements.txt --target=dist/data  --find-links ../../../ak-py/dist
-      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist agentkernel[api,crewai,redis,test] || true
+      uv pip install --force-reinstall --target=dist/data --find-links ../../../ak-py/dist agentkernel[api,crewai,redis,multimodal,test] || true
     fi
     cp -r app.py config.yaml dist/data
     popd || exit 1
