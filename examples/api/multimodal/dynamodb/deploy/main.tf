79f123062b450749feaf621a049230797174f19c
yl-nuwan <nuwan.udara@yaalalabs.com>
2026-06-08 14:59:03 +0530

    formated the documant

--- examples/api/multimodal/dynamodb/deploy/main.tf
+++ examples/api/multimodal/dynamodb/deploy/main.tf
@@ -5,23 +5,23 @@ module "serverless_agents" {
   version = "0.3.3"
 
   # Basic lambda configuration
-  product_alias                = var.product_alias
-  env_alias                    = var.env_alias
-  module_name                  = var.module_name
+  product_alias                           = var.product_alias
+  env_alias                               = var.env_alias
+  module_name                             = var.module_name
   create_dynamodb_multimodal_memory_table = true
-  product_display_name         = "Agent Kernel OpenAI Multimodal with DynamoDB"
-  region                       = var.region
+  product_display_name                    = "Agent Kernel OpenAI Multimodal with DynamoDB"
+  region                                  = var.region
 
   # Request handler configuration
   request_handler = {
-    function_description         = "Agent Kernel OpenAI Multimodal with DynamoDB"
-    function_name                = "mm-ddb"
-    handler_path                 = "lambda.handler" # Set dummy valid file to skip validaton inside zip
-    module_name                  = var.module_name
-    package_path                 = "../dist"
-    package_type                 = "Image"
-    memory_size                  = 2048
-    timeout                      = 60
+    function_description = "Agent Kernel OpenAI Multimodal with DynamoDB"
+    function_name        = "mm-ddb"
+    handler_path         = "lambda.handler" # Set dummy valid file to skip validaton inside zip
+    module_name          = var.module_name
+    package_path         = "../dist"
+    package_type         = "Image"
+    memory_size          = 2048
+    timeout              = 60
     environment_variables = {
       "OPENAI_API_KEY" = var.openai_api_key
     }
