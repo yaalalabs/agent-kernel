1fc3f192bb7688d0a44196c1f5d8bcf6c751fc83
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-07-07 17:59:39 +0530

    graceful fix update

--- ak-py/src/agentkernel/deployment/aws/containerized/ecs_io_handler.py
+++ ak-py/src/agentkernel/deployment/aws/containerized/ecs_io_handler.py
@@ -41,12 +41,13 @@ class ECSIOHandler:
                         handlers=[ECSQueueRequestHandler()]
                     ),  # lambda needed here to wrap the function so that it turns into a callable, because otherwise the rest api will be run here itself
                     thread_name="rest-api",
+                    stop_all_on_failure=True,
+                    graceful=True
                 ),
                 ThreadRunner.Task(
                     execution_function=lambda: ECSOutputConsumer.run(),
                     thread_name="output-queue-consumer",
                     stop_all_on_failure=True,
-                    graceful=True,
                 ),
             ],
             max_workers=2,
