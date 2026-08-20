5615697f902f1893dbe6366d2a9d4f3f8daf292f
Lakindu Induwara Kumara <lakindu.kumara@yaalalabs.com>
2026-08-20 20:54:22 +0530

    updated deploy.sh and build.sh files to get from local wheel

--- examples/aws-containerized/openai-stream/deploy/deploy.sh
+++ examples/aws-containerized/openai-stream/deploy/deploy.sh
@@ -10,8 +10,7 @@ create_deployment_package() {
     if [[ ${1-} != "local" ]]; then
       uv pip install -r requirements.txt --target=dist/data
     else
-      uv pip install -r requirements.txt --target=dist/data --find-links ../../../ak-py/dist
-      uv pip install --force-reinstall --no-deps --no-index --target=dist/data --find-links ../../../ak-py/dist agentkernel[openai,api,aws,auth,test] || true
+      uv pip install -r requirements.txt --target=dist/data --find-links ../../../ak-py/dist --upgrade-package agentkernel || true
     fi
     cp -r app.py config.yaml dist/data
     popd || exit 1
